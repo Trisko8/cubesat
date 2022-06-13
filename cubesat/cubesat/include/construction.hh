14307f7d6ebab8409116ef6324e0048c959853ae
Trisko8 <102881474+Trisko8@users.noreply.github.com>
2022-06-13 16:39:50 +0200

    Add files via upload

---
+++ cubesat/cubesat/include/construction.hh
@@ -0,0 +1,47 @@
+#ifndef CONSTRUCTION_HH
+#define CONSTRUCTION_HH
+
+#include "G4VUserDetectorConstruction.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4LogicalVolume.hh"
+#include "G4Box.hh"
+#include "G4PVPlacement.hh"
+#include "G4NistManager.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4OpticalSurface.hh"
+#include "G4LogicalBorderSurface.hh"
+#include "G4LogicalSkinSurface.hh"
+
+#include "detector.hh"
+
+class MyDetectorConstruction : public G4VUserDetectorConstruction
+{
+public:
+    MyDetectorConstruction();
+    ~MyDetectorConstruction();
+
+     G4LogicalVolume *GetScoringVolume() const { return fScoringVolume; }
+
+    virtual G4VPhysicalVolume *Construct();
+
+private:
+    G4Box *solidWorld, *solidLayer, *solidWallz, *solidWallx, *solidScintillator;
+    G4LogicalVolume *logicWorld, *logicLayer, *logicWallz, *logicWallx, *logicScintillator;
+    G4VPhysicalVolume *physWorld, *physLayerUp, *physLayerDown, *physWallzLeft, *physWallzRight, *physWallxLeft, *physWallxRight, *physScintillator, *physScintillatorT, *physScintillatorR, *physScintillatorRT;
+
+    G4Material *worldMat, *ScintillatorMat, *Al_6061;
+
+    void DefineMaterials();
+
+    void ConstructScintillator();
+
+    virtual void ConstructSDandField();
+
+    G4LogicalVolume *fScoringVolume;
+
+    G4double xWorld, yWorld, zWorld;
+
+    G4OpticalSurface *mirrorSurface;
+};
+
+#endif
---
+++ cubesat/cubesat/include/generator.hh
@@ -0,0 +1,28 @@
+#ifndef GENERATOR_HH
+#define GENERATOR_HH
+
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4GeneralParticleSource.hh"
+
+#include "G4ParticleGun.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4ParticleTable.hh"
+#include "G4IonTable.hh"
+#include "G4ChargedGeantino.hh"
+
+#include <math.h>
+
+
+class MyPrimaryGenerator : public G4VUserPrimaryGeneratorAction
+{
+public:
+    MyPrimaryGenerator();
+    ~MyPrimaryGenerator();
+
+    virtual void GeneratePrimaries(G4Event*);
+
+private:
+    G4GeneralParticleSource *fParticleGun;
+};
+
+#endif
